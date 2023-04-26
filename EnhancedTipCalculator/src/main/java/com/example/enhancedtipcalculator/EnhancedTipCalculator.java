03e752977f7ca9a0f734f7e224e6f3d7ed6f3d14
Islam Momukeev <islambek.momukeev_2026@ucentralasia.org>
2023-04-26 15:27:31 +0600

    first update

--- EnhancedTipCalculator/src/main/java/com/example/enhancedtipcalculator/EnhancedTipCalculator.java
+++ EnhancedTipCalculator/src/main/java/com/example/enhancedtipcalculator/EnhancedTipCalculator.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class EnhancedTipCalculator extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(EnhancedTipCalculator.class.getResource("EnhancedTipCalculator.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("EnhancedTipCalculator");
         stage.setScene(scene);
         stage.show();
     }
